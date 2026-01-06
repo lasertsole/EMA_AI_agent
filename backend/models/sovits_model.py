24201aad11c7db8a2c11d13cac57391404c83aaf
MOYE <56973455+lasertsole@users.noreply.github.com>
2026-01-06 11:31:24 +0800

    Update sovits_model.py

--- backend/models/sovits_model.py
+++ backend/models/sovits_model.py
@@ -1,1 +1,35 @@
+import requests
+from pydantic import BaseModel
 
+class TTS_Request(BaseModel):
+    text: str = None
+    text_lang: str = None
+    ref_audio_path: str = None
+    aux_ref_audio_paths: list = None
+    prompt_lang: str = None
+    prompt_text: str = ""
+    top_k: int = 15
+    top_p: float = 1
+    temperature: float = 1
+    text_split_method: str = "cut5"
+    batch_size: int = 1
+    batch_threshold: float = 0.75
+    split_bucket: bool = True
+    speed_factor: float = 1.0
+    fragment_interval: float = 0.3
+    seed: int = -1
+    media_type: str = "wav"
+    streaming_mode: Union[bool, int] = False
+    parallel_infer: bool = True
+    repetition_penalty: float = 1.35
+    sample_steps: int = 32
+    super_sampling: bool = False
+    overlap_length: int = 2
+    min_chunk_length: int = 16
+
+url = "http://127.0.0.1:9880/"
+  
+def getTTSSound(request: TTS_Request):
+    response = requests.post(url, data=json.dumps(data), verify=True)
+    print(response)
+    return response
