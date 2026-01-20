24dad2ced8725fb9687883be30d0af7caa48dd60
MOYE <56973455+lasertsole@users.noreply.github.com>
2026-01-20 17:19:54 +0800

    update core_graph.py

--- tools/rag/core_graph.py
+++ tools/rag/core_graph.py
@@ -4,6 +4,8 @@ from langchain_core.prompts import PromptTemplate
 from langgraph.checkpoint.memory import InMemorySaver
 from langchain.agents import create_agent
 from pydantic import BaseModel, Field
+from typing import TypedDict, Annotated
+from langgraph.graph import StateGraph, START, END
 
 # 线程记忆功能
 checkpoint = InMemorySaver()
@@ -13,7 +15,7 @@ researcher_systemPrompt = """
 你不需要利用自身知识库回答问题，所有信息必须来源于外部工具。
 """
 #生成agent对象
-researcher_agent = create_agent(
+researcher = create_agent(
     model=reasoner_model,
     system_prompt = researcher_systemPrompt
 )
@@ -31,7 +33,7 @@ rewriter_systemPrompt_Template = PromptTemplate(
 
 class ReWriteQueryListOutput(BaseModel):
     """字符串列表输出结构"""
-    queries: list[str] = Field(
+    reQueries: list[str] = Field(
         description="生成的重写后的查询列表",
         examples=[[
             "当前西瓜市场零售价格是多少钱一斤？",
@@ -42,9 +44,39 @@ class ReWriteQueryListOutput(BaseModel):
 
 rewriter = rewriter_systemPrompt_Template | base_model.with_structured_output(ReWriteQueryListOutput)
 
+
+
+class GraphState(TypedDict):
+    query: str # 原问题
+    reQueries: list[str] # 重写后问题列表
+    res: str # 查询结果
+
+async def rewriter_node(state: GraphState):
+    result = await rewriter.ainvoke({"query": "西瓜多少元一斤?"})
+    print(result.reQueries)
+    print(state["query"])
+    return {"query": result.reQueries[0], "reQueries": result.reQueries}
+
+async def researcher_node(state: GraphState):
+    print(state["reQueries"])
+    return {"res": "123"}
+
+workflow = StateGraph(GraphState)
+workflow.add_node("rewriter", rewriter_node)
+workflow.add_node("researcher", researcher_node)
+workflow.add_edge(START, "rewriter")
+workflow.add_edge("rewriter", "researcher")
+workflow.add_edge("researcher", END)
+graph = workflow.compile()
+
+
 # 运行异步主函数
 if __name__ == "__main__":
     async def test():
         result = await rewriter.ainvoke({"query": "西瓜多少元一斤?"})
-        print(result.queries)
-    asyncio.run(test())
+        print(result.reQueries)
+
+    async def test1():
+        result = await graph.ainvoke({"query": "西瓜多少元一斤?"})
+        print(result)
+    asyncio.run(test1())
