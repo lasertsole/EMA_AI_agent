94c2fb8408a1b3bb8ea6d15cbed79fb93a69223d
lasertsole <3132225629@qq.com>
2026-02-26 22:46:55 +0800

    update streamlit

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ AI 聊天机器人 - Streamlit 异步实现
     $  python -m streamlit run main.py
 """
 import os
+import re
 import streamlit as st
 from typing import Any
 from agent import agent
@@ -16,14 +17,17 @@ from dotenv import load_dotenv
 from typing import AsyncGenerator
 from langchain.messages import AIMessageChunk
 from models import TTS_Request, fetchTTSSound
+from langchain_core.messages import HumanMessage, AIMessage
 
 env_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), './.env')
 load_dotenv(env_path, override=True)
 is_stream = os.getenv("IS_STREAM")
 
+ai_reply_prefix = "橘雪莉:"
+
 async def async_generator(message_list, config)-> AsyncGenerator[str, None]:
     if is_stream == 'True':
-        yield "橘雪莉:"
+        yield ai_reply_prefix
         async for chunk in agent.astream({"messages": message_list}, config=config, stream_mode="messages"):
             msg_chunk: AIMessageChunk = chunk[0]
             event_chunk: dict[str, Any] = chunk[1]
@@ -34,36 +38,44 @@ async def async_generator(message_list, config)-> AsyncGenerator[str, None]:
                 yield msg_chunk.content
     else:
         result = await agent.ainvoke({"messages": message_list}, config=config)
-        yield "橘雪莉:" + result["messages"][-1].content
+        yield ai_reply_prefix + result["messages"][-1].content
 
 
 if __name__ == "__main__":
     config = {"configurable": {"thread_id": 1}}
 
-    if "messages" not in st.session_state:
-        st.session_state.messages = []
+    if 'messages' in agent.get_state(config).values:
+        for message in agent.get_state(config).values['messages']:
+            if isinstance(message, HumanMessage):
+                with st.chat_message("user"):
+                    content = "远野汉娜:" + message.content
+                    st.markdown(content)
 
-    ai_message = st.chat_message("assistant")
-    ai_message.write("橘雪莉:汉娜さん，来茶间聊天吧！")
-    for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
+            elif isinstance(message, AIMessage):
+                with st.chat_message("assistant"):
+                    content = ai_reply_prefix + message.content
+                    st.markdown(content)
+    else:
+        ai_message = st.chat_message("assistant")
+        ai_message.write(ai_reply_prefix + "汉娜さん，来茶间聊天吧！")
 
     human_input = st.chat_input("请输入对话内容")
     if human_input:
         with st.chat_message("user"):
             content = "远野汉娜:" + human_input
             st.markdown(content)
-            st.session_state.messages.append({"role": "user", "content": content})
         
         message_list = [human_input]
         with st.chat_message("assistant"):
             stream = async_generator(message_list, config)
             content = st.write_stream(stream)
-            st.session_state.messages.append({"role": "assistant", "content": content})
 
             # 生成语音,当生成失败时跳过生成
             try:
+                # 去除多余字符
+                content = content[len(ai_reply_prefix):-1]
+                content = re.sub(r'[（\(].*?[）\)]', ' ', content)
+
                 audio_requires = TTS_Request(text=content, text_lang = "zh")
                 response = fetchTTSSound(audio_requires)
                 if response is not None:
