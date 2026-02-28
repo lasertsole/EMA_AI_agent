ba81b7a96f1dbdf282f0c3bb75203ee85b73e059
lasertsole <3132225629@qq.com>
2026-02-28 23:58:35 +0800

    update streamlit

--- main.py
+++ main.py
@@ -10,24 +10,22 @@ AI 聊天机器人 - Streamlit 异步实现
 """
 import os
 import re
-import streamlit as st
 from typing import Any
+import streamlit as st
 from agent import agent
-from collections import deque
+from pathlib import Path
 from dotenv import load_dotenv
 from typing import AsyncGenerator
-from utils import Chat, add_chats, get_chats, add_files
+from utils import Chat, File, FileType, ChatStorage
 from langchain.messages import AIMessageChunk
 from models import TTS_Request, fetchTTSSound
-from streamlit_local_storage import LocalStorage
-
-from utils import add_files, FileType
 
 env_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), './.env')
 load_dotenv(env_path, override=True)
 is_stream = os.getenv("IS_STREAM")
 
 ai_reply_prefix = "橘雪莉:"
+chat_storage = ChatStorage(chats_maxlen = 20)
 
 async def async_generator(text, config)-> AsyncGenerator[str, None]:
     """
@@ -58,21 +56,21 @@ def filter_content_for_tts(content: str) -> str:
 
 if __name__ == "__main__":
     config = {"configurable": {"thread_id": 1}}
-    localS = LocalStorage()
-    chat_history_maxlen = 20
 
-    saved_data = localS.getItem("chat_history")
-    if saved_data is None:
-        saved_data = deque(maxlen = chat_history_maxlen)
-        saved_data.append({"role": "assistant", "content": f"{ai_reply_prefix}汉娜さん，来茶间聊天吧！"})
-    else:
-        saved_data = deque(saved_data, maxlen = chat_history_maxlen)
+    chat_list: list[Chat] = chat_storage.get_chats()
+    if len(chat_list) == 0:
+        hello_chat = Chat(role="assistant", content = f"{ai_reply_prefix}汉娜さん，来茶间聊天吧！")
+        chat_storage.add_chat(hello_chat)
+        chat_list.append(hello_chat)
 
-    for message in saved_data:
-        with st.chat_message(message["role"], avatar=f"./src/avatar/{message["role"]}.jpg"):
-            st.markdown(message["content"])
-            if "audio" in message:
-                st.audio(data=message["audio"], format="audio/ogg")
+    for chat in chat_list:
+        with st.chat_message(chat.role, avatar=f"./src/avatar/{chat.role}.jpg"):
+            st.markdown(chat.content)
+            if chat.audio_path_list:
+                for file_path in chat.audio_path_list:
+                    if Path(file_path).exists():
+                        with open(file_path, "rb") as f:
+                            st.audio(data=f.read(), format="audio/ogg")
 
     # 用户输入
     human_input = st.chat_input(
@@ -90,16 +88,13 @@ if __name__ == "__main__":
             for file in files:
                 st.image(file)
 
-            saved_data.append({"role": "user", "content": content})
-            add_chats([Chat(role="user", content=content)])
+            chat_storage.add_chat(Chat(role="user", content=content))
 
         with st.chat_message("assistant", avatar="./src/avatar/assistant.jpg"):
             stream = async_generator(text, config)
             content = st.write_stream(stream)
 
-            message_obj = {"role": "assistant", "content": content}
-            saved_data.append(message_obj)
-
+            file: File
             with st.spinner("正在生成语音..."):
                 # 生成语音,当生成失败时跳过生成
                 try:
@@ -110,9 +105,8 @@ if __name__ == "__main__":
                     response = fetchTTSSound(audio_requires)
                     if response is not None:
                         st.audio(data=response.content, format="audio/ogg")
-                        add_files([{"content":response.content, "type":FileType.AUDIO}])
+                        file = {"content":response.content, "type":FileType.AUDIO}
                 except Exception as e:
                     pass
 
-
-        localS.setItem("chat_history", list(saved_data))
+            chat_storage.add_chat(Chat(role="assistant", content=content), files=[file])
--- utils/chats_storage.py
+++ utils/chats_storage.py
@@ -22,7 +22,7 @@ class Chat(BaseModel):
     content: str
     audio_path_list: Optional[List[str]] =  None
     image_path_list: Optional[List[str]] =  None
-    timestamp: float = time.time()
+    timestamp: float = time.time_ns()
 
 class File(TypedDict):
     content: bytes
@@ -30,91 +30,89 @@ class File(TypedDict):
 
 # 聊天记录存储类
 class ChatStorage:
-    chats_maxlen: int = 20
-    chats_deque: deque[Chat]
+    _chats_deque: deque[Chat]
 
     def __init__(self, chats_maxlen: int = 20):
-        self.chats_maxlen = chats_maxlen
-        self.chats_deque = deque(maxlen=self.chats_maxlen)
-
-    def delete_file_from_chats(self, chats_list: List[Chat]):
-        # 将聊天记录对应的文件删除
-        for chat in chats_list:
-            for file_name in chat.audios_name:
-                Path(file_name).unlink()
-            for file_name in chat.images_name:
-                Path(file_name).unlink()
-
-    def storage_chats_deque(self, chats_deque: Deque[Chat]):
-        SESSION_FOLDER.mkdir(parents=True, exist_ok=True)
-
-        # 将新聊天记录列表写回文件
-        with open(CHATS_STORAGE_FILE, 'w', encoding='utf-8') as f:
-            for chat in chats_deque:
-                f.write(json.dumps(chat, ensure_ascii=False) + "\n")
-
-
-    def get_new_chats_and_delete_old_chats(self) -> List[Chat]:
-        SESSION_FOLDER.mkdir(parents=True, exist_ok=True)
-
         # 获取已持久化的聊天记录
         chats_list: List[Chat] = []
+
+        SESSION_FOLDER.mkdir(parents=True, exist_ok=True)
         with open(CHATS_STORAGE_FILE, 'r', encoding='utf-8') as f:
             for line in f:
-                chats_list.append(json.loads(line.strip()))
+                chats_list.append(Chat(**json.loads(line.strip())))
 
         # 根据时间戳排序聊天记录,从新到旧
         chats_list.sort(key=lambda x: x.timestamp)
 
         # 将新聊天记录装入双向列表
-        self.chats_deque = deque(chats_list[-self.chats_maxlen:], maxlen=self.chats_maxlen)
+        self._chats_deque = deque(chats_list[-chats_maxlen:], maxlen=chats_maxlen)
 
         # 将旧聊天记录装入旧聊天记录列表
-        old_chats_list = chats_list[self.chats_maxlen:]
+        old_chats_list = chats_list[:-chats_maxlen]
 
         # 将聊天记录对应的文件删除
         self.delete_file_from_chats(old_chats_list)
 
         # 将新聊天记录列表写回文件
-        self.storage_chats_deque(self.chats_deque)
+        self.storage_chats_deque(self._chats_deque)
 
-        return list(self.chats_deque)
+    def get_chats(self) -> List[Chat]:
+        return list(self._chats_deque)
 
-    def add_chat(self, new_chat: Chat, files: List[File]):
-        # 获取最旧的聊天信息
-        old_chat = self.chats_deque[0]
-
+    def delete_file_from_chats(self, chats_list: List[Chat]):
         # 将聊天记录对应的文件删除
-        self.delete_file_from_chats([old_chat])
+        for chat in chats_list:
+            for file_path in chat.audio_path_list:
+                try:
+                    if Path(file_path).exists():
+                        Path(file_path).unlink()
+                except Exception as e:
+                    pass
+            for file_path in chat.image_path_list:
+                try:
+                    if Path(file_path).exists():
+                        Path(file_path).unlink()
+                except Exception as e:
+                    pass
 
+    def storage_chats_deque(self, _chats_deque: Deque[Chat]):
+        SESSION_FOLDER.mkdir(parents=True, exist_ok=True)
+
+        # 将新聊天记录列表写回文件
+        with open(CHATS_STORAGE_FILE, 'w', encoding='utf-8') as f:
+            for chat in _chats_deque:
+                f.write(json.dumps(chat.model_dump(), ensure_ascii=False) + "\n")
+
+    def add_chat(self, new_chat: Chat, files: Optional[List[File]] = None):
         # 将新聊天记录装入双向列表
-        self.chats_deque.append(new_chat)
+        self._chats_deque.append(new_chat)
 
         # 将文件写入文件夹
         file_path_list = []
         audio_path_list = []
         image_path_list = []
-        for file in files:
-            folder_path = SESSION_FOLDER / file["type"].value
-            file_path = folder_path / str(time.time_ns())
-            file_path = file_path.as_posix()
+        if files:
+            for file in files:
+                folder_path = SESSION_FOLDER / file["type"].value
+                file_path = folder_path / str(time.time_ns())
+                file_path = file_path.as_posix()
 
-            match file["type"]:
-                case FileType.AUDIO:
-                    file_path = file_path + '.wav'
-                    audio_path_list.append(file_path)
-                case FileType.IMAGE:
-                    file_path = file_path + '.jpg'
-                    image_path_list.append(file_path)
-                case _:
-                    raise ValueError("Invalid file type")
+                match file["type"]:
+                    case FileType.AUDIO:
+                        file_path = file_path + '.wav'
+                        audio_path_list.append(file_path)
+                    case FileType.IMAGE:
+                        file_path = file_path + '.jpg'
+                        image_path_list.append(file_path)
+                    case _:
+                        raise ValueError("Invalid file type")
 
-            # 确保目录存在
-            Path(folder_path).mkdir(parents=True, exist_ok=True)
+                # 确保目录存在
+                Path(folder_path).mkdir(parents=True, exist_ok=True)
 
-            with open(file_path, "wb") as f:
-                f.write(file["content"])
-                file_path_list.append(file_path)
+                with open(file_path, "wb") as f:
+                    f.write(file["content"])
+                    file_path_list.append(file_path)
 
         # 将文件路径写入新聊天记录
         new_chat.audio_path_list = audio_path_list
@@ -122,4 +120,4 @@ class ChatStorage:
 
         # 将聊天记录追加到文件末尾
         with open(CHATS_STORAGE_FILE, 'a', encoding='utf-8') as f:
-            f.write(json.dumps(new_chat, ensure_ascii=False) + "\n")
+            f.write(json.dumps(new_chat.model_dump(), ensure_ascii=False) + "\n")
