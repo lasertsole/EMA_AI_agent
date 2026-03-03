9bd2ad0b64cc908f043400c23e5f36bc8cf310b6
lasertsole <3132225629@qq.com>
2026-03-03 19:28:36 +0800

    use native rag tool to instead of DIY rag tool

--- tools/agentic_rag/native/core.py
+++ tools/agentic_rag/native/core.py
@@ -9,7 +9,8 @@ from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.documents import Document
 from langchain.chat_models import init_chat_model
-from langchain_classic.retrievers import MultiVectorRetriever
+from langchain_classic.retrievers import MultiVectorRetriever, MultiQueryRetriever, EnsembleRetriever
+from langchain_community.retrievers import BM25Retriever
 from langchain_classic.retrievers.multi_vector import SearchType
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import TextLoader
@@ -26,51 +27,52 @@ api_key = os.getenv("CHAT_API_KEY")
 api_name = os.getenv("CHAT_API_NAME")
 model_provider = os.getenv("CHAT_MODEL_PROVIDER")
 
+
+def format_doc(doc: Document, separators: list) -> Document:
+    for sep in separators:
+        doc.page_content = doc.page_content.strip(sep).strip()
+    return doc
+
 # 设置文档唯一id
 id_key = "doc_id"
 
 # 如果output文件已存在，则不重复持久化
 indexFolderPath = current_dir / "output"
 store = LocalFileStore(indexFolderPath)
-if not indexFolderPath.exists():
-    # 生成摘要模型对象
-    summarize_model = init_chat_model(
-        model_provider = model_provider,
-        model = api_name,
-        api_key = api_key,
-        temperature = 0.8,
-        max_retries = 2
-    )
 
+separators = ["\n\n\n"]
+# 创建分词器
+text_splitter = RecursiveCharacterTextSplitter(
+    separators=separators,
+    is_separator_regex=False,
+    chunk_size=1,
+    chunk_overlap=0
+)
+
+# 加载原文档
+info_path = current_dir / "input/allCharacters.txt"
+loader = TextLoader(info_path, encoding="utf-8")
+documents = loader.load()
+docs = text_splitter.split_documents(documents)
+docs = [format_doc(doc, separators) for doc in docs]
+
+# 生成对话模型
+llm = init_chat_model(
+    model_provider=model_provider,
+    model=api_name,
+    api_key=api_key,
+    temperature=0.8,
+    max_retries=2
+)
+
+if not indexFolderPath.exists():
     # 构造生成摘要链
     summarize_chain = (
             {"doc": lambda x: x.page_content}
             | ChatPromptTemplate.from_template("Summarize the following document:\n\n{doc}")
-            | summarize_model
+            | llm
             | StrOutputParser()
     )
-
-    separators = ["\n\n\n"]
-
-    def format_doc(doc: Document, separators: list) -> Document:
-        for sep in separators:
-            doc.page_content = doc.page_content.strip(sep).strip()
-        return doc
-
-    # 创建分词器
-    text_splitter = RecursiveCharacterTextSplitter(
-        separators = separators,
-        is_separator_regex = False,
-        chunk_size = 1,
-        chunk_overlap = 0
-    )
-
-    # 加载原文档
-    info_path = current_dir / "input/allCharacters.txt"
-    loader = TextLoader(info_path, encoding="utf-8")
-    documents = loader.load()
-    docs = text_splitter.split_documents(documents)
-    docs = [format_doc(doc, separators) for doc in docs]
 
     # 批量生成摘要
     summaries = summarize_chain.batch(docs, {"max_concurrency": 5})
@@ -89,7 +91,7 @@ if not indexFolderPath.exists():
     vector_store = FAISS.from_documents(docs, embedding = embed_model)
 
     # 创建多向量召回器
-    retriever = MultiVectorRetriever(
+    vector_retriever = MultiVectorRetriever(
         vectorstore = vector_store,
         byte_store = store,
         id_key = id_key,
@@ -97,10 +99,10 @@ if not indexFolderPath.exists():
     )
 
     # 将摘要持久化
-    retriever.vectorstore.add_documents(summary_docs)
+    vector_retriever.vectorstore.add_documents(summary_docs)
 
     # 将doc_id和doc一一对应，并存入docstore
-    retriever.docstore.mset(list(zip(doc_ids, docs)))
+    vector_retriever.docstore.mset(list(zip(doc_ids, docs)))
 
     # 持久化FAISS向量文件
     vector_store.save_local(indexFolderPath.as_posix())
@@ -114,12 +116,16 @@ else:
     )
 
     # 创建多向量召回器
-    retriever = MultiVectorRetriever(
+    vector_retriever = MultiVectorRetriever(
         vectorstore = vector_store,
         byte_store = store,
         id_key = id_key,
         search_type = SearchType.similarity_score_threshold
     )
+
+vector_retriever = MultiQueryRetriever.from_llm(retriever = vector_retriever, llm = llm)
+BM25_retriever = BM25Retriever.from_documents(documents = docs)
+retriever = EnsembleRetriever(retrievers=[vector_retriever, BM25_retriever], weights=[0.6, 0.4])
 
 def _query_background_info(query:str) -> List[str]:
     ### 检验参数 ###
@@ -136,3 +142,6 @@ def _query_background_info(query:str) -> List[str]:
 
 #query_background_info = Tool(name="query_background_info", func=_query_background_info, description="""当需要回答 魔法少女的魔女审批 有关知识时调用此工具，
 # 输入为具体问题，输出为知识库检索到的答案""")
+
+if __name__ == "__main__":
+    print(_query_background_info("宝生玛格"))
