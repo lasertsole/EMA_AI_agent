0bfb74ce7a2561a2b84fe62e8f3cda6173fd6cc5
MOYE <56973455+lasertsole@users.noreply.github.com>
2026-03-11 15:54:17 +0800

    update start.sh

--- start.sh
+++ start.sh
@@ -5,7 +5,20 @@ if [ -f .env ]; then
   echo "检测到.env文件，正在提取配置..."
   GPT_SOVITS_DIR=$(grep "GPT_SOVITS_DIR" .env | awk -F '=' '{print $2}' | sed 's/^[[:space:]]*//;s/[[:space:]]*$//')
   if [ -n "$GPT_SOVITS_DIR" ]; then
-    echo "成功提取 TTS目录地址GPT_SOVITS_DIR: $GPT_SOVITS_DIR"
+    GPT_SOVITS_DIR="${GPT_SOVITS_DIR//\\//}" # 将反斜杠转化为正斜杠
+    CURRENT_DIR=$(pwd)
+    CONFIG_PATH="${CURRENT_DIR}/config/tts_infer.yaml"
+    INTERPRETER_PATH="${GPT_SOVITS_DIR}/runtime/python.exe"
+    API_PATH="${GPT_SOVITS_DIR}/api_v2.py"
+
+    FULL_COMMAND=(
+      "$INTERPRETER_PATH"
+      "$API_PATH"
+      "-a" "127.0.0.1"
+      "-p" "9880"
+      "-c" "$CONFIG_PATH"
+    )
+    "${FULL_COMMAND[@]}" || echo "执行启动TTS服务的脚本时发生错误，将跳过TTS服务"
   fi
 else
   echo ".env 文件不存在，跳过TTS服务"
